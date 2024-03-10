6f464d9716bc3f4b5c0cdcb25f8a8739bd26744c
Daniel Thiem <info@daniel-thiem.de>
2024-03-10 21:15:35 +0100

    Cleaned up CreateTeam.vue

--- frontend-vite/src/components/team/CreateTeam.vue
+++ frontend-vite/src/components/team/CreateTeam.vue
@@ -3,9 +3,10 @@ import {onMounted, ref} from "vue";
 import {createTeam} from "@/utils/api/teams_api";
 import {useReportStore} from "@/utils/edit_report_store";
 import type {Team} from "@/types/team_types";
+
 const store = useReportStore()
 const props = defineProps<{
-  rough_team_name?:string
+  rough_team_name?: string
 }>()
 
 const emit = defineEmits<{
@@ -13,39 +14,46 @@ const emit = defineEmits<{
   (e: 'on_cancel'): void
 }>()
 
-onMounted(()=> {
+onMounted(() => {
   new_team_name.value = props.rough_team_name || ""
 })
 const new_team_name = ref("")
 const is_loading = ref(false)
+
 async function send_new_team_to_server() {
   if (new_team_name.value != "") {
-
     is_loading.value = true
-    let data = await createTeam(new_team_name.value)
+    createTeam(new_team_name.value)
         .catch((error) => {
           store.newError(error)
-          return undefined
         })
-    is_loading.value = false
-    emit('on_new_team',data as Team)
+        .then((data) => {
+          emit('on_new_team', data as Team)
+
+        })
+        .finally(() => {
+          is_loading.value = false
+        })
+
   }
 }
 </script>
 
 <template>
-  <label for="new_team_name">{{ $t('teamSelect.newTeamName') }}</label><br>
-  <InputText id="new_team_name" :disabled="is_loading" v-model="new_team_name" /><br>
-  <div class="flex flex-row">
-    <div class="m-2">
-      <Button @click="send_new_team_to_server">{{ $t('general.submit') }}</Button>
-    </div>
-    <div class="m-2">
-      <Button @click="emit('on_cancel')">{{ $t('general.cancel') }}</Button>
+  <div class="w-fill shadow-xl p-2 bg-gray-100 border-gray-500 rounded-xl flex-col text-center mb-4">
+    <label for="new_team_name">{{ $t('teamSelect.newTeamName') }}</label><br>
+    <InputText id="new_team_name" :disabled="is_loading" v-model="new_team_name"/>
+    <br>
+    <div class="flex flex-row justify-center">
+      <div class="m-2">
+        <Button @click="send_new_team_to_server">{{ $t('general.submit') }}</Button>
+      </div>
+      <div class="m-2">
+        <Button @click="emit('on_cancel')">{{ $t('general.cancel') }}</Button>
+      </div>
     </div>
   </div>
 </template>
-
 
 
 <style scoped>
