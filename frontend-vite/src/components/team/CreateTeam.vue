0033f6067672fc5c02e3f929da13a05cb8432f69
Daniel Thiem <info@daniel-thiem.de>
2024-07-12 12:28:09 +0200

    CreateTeam now prevents Names similar to Squads and Amalgamations

--- frontend-vite/src/components/team/CreateTeam.vue
+++ frontend-vite/src/components/team/CreateTeam.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
-import {onMounted, ref} from "vue";
+import {computed, onMounted, ref, watch} from "vue";
 import {createTeam} from "@/utils/api/teams_api";
 import {useReportStore} from "@/utils/edit_report_store";
 import type {Team} from "@/types/team_types";
+import {useI18n} from "vue-i18n";
+import {watchDebounced} from "@vueuse/core";
 
 const store = useReportStore()
 const props = defineProps<{
@@ -37,6 +39,58 @@ async function send_new_team_to_server() {
 
   }
 }
+
+class TeamNameWarning {
+  ignore: boolean = false
+  constructor(public message: string) {
+  }
+}
+const {t} = useI18n()
+class TeamNameTooShortWarning extends TeamNameWarning {
+  constructor() {
+    super(t('teamSelect.errorNameTooShort'))
+  }
+}
+
+class TeamNameIndicatesAmalgamationWarning extends TeamNameWarning {
+  constructor() {
+    super(t('teamSelect.errorNameIndicatesAmalgamation'))
+  }
+}
+
+class TeamNameIndicatesSquadWarning extends TeamNameWarning {
+  constructor() {
+    super(t('teamSelect.errorNameIndicatesSquad'))
+  }
+}
+
+
+const invalidTeamNameWarnings = ref<TeamNameWarning[]>([])
+
+watchDebounced(new_team_name,() => {
+  const teamNameTooShort = new_team_name.value.length < 3;
+  if (teamNameTooShort &&
+      invalidTeamNameWarnings.value.findIndex(it => it instanceof TeamNameTooShortWarning) === -1) {
+    invalidTeamNameWarnings.value.push(new TeamNameTooShortWarning())
+  } else if (new_team_name.value.length >= 3) {
+    invalidTeamNameWarnings.value = invalidTeamNameWarnings.value.filter(it => !(it instanceof TeamNameTooShortWarning))
+  }
+  const teamNameMatchesAmalgamation = new_team_name.value.match(/\//)
+  if (teamNameMatchesAmalgamation &&
+      invalidTeamNameWarnings.value.findIndex(it => it instanceof TeamNameIndicatesAmalgamationWarning) === -1) {
+    invalidTeamNameWarnings.value.push(new TeamNameIndicatesAmalgamationWarning())
+  } else if (!teamNameMatchesAmalgamation) {
+    invalidTeamNameWarnings.value = invalidTeamNameWarnings.value.filter(it => !(it instanceof TeamNameIndicatesAmalgamationWarning))
+  }
+
+  const teamNameMatchesSquad = new_team_name.value.match(/\s[a-zA-Z]($|\s+$)/);
+  if (teamNameMatchesSquad &&
+      invalidTeamNameWarnings.value.findIndex(it => it instanceof TeamNameIndicatesSquadWarning) === -1) {
+    invalidTeamNameWarnings.value.push(new TeamNameIndicatesSquadWarning())
+  } else if (!teamNameMatchesSquad) {
+    invalidTeamNameWarnings.value = invalidTeamNameWarnings.value.filter(it => !(it instanceof TeamNameIndicatesSquadWarning))
+  }
+}, {debounce: 200, maxWait:400})
 </script>
 
 <template>
@@ -44,9 +98,16 @@ async function send_new_team_to_server() {
     <label for="new_team_name">{{ $t('teamSelect.newTeamName') }}</label><br>
     <InputText id="new_team_name" :disabled="is_loading" v-model="new_team_name"/>
     <br>
+    <div v-if="invalidTeamNameWarnings.length > 0" class="text-red-500">
+      <ul>
+        <li v-for="warning in invalidTeamNameWarnings">{{ warning.message }}</li>
+      </ul>
+    </div>
     <div class="flex flex-row justify-center">
       <div class="m-2">
-        <Button @click="send_new_team_to_server">{{ $t('general.submit') }}</Button>
+        <Button @click="send_new_team_to_server"
+                :disabled="invalidTeamNameWarnings.length > 0"
+        >{{ $t('general.submit') }}</Button>
       </div>
       <div class="m-2">
         <Button @click="emit('on_cancel')">{{ $t('general.cancel') }}</Button>
